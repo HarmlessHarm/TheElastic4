2462f84445b4adfe1765b880d6b72bc0cdc9b6ad
Harm Manders <harmmanders@gmail.com>
2018-10-22 22:38:39 +0200

    Question data is loaded in elasticsearch db

--- elasticapp/index_data.py
+++ elasticapp/index_data.py
@@ -1,1 +1,37 @@
 from elasticsearch import Elasticsearch, helpers
+
+from elasticapp.constants import *
+from elasticapp.data import *
+
+def main():
+	es = Elasticsearch()
+
+	es.indices.delete(index=Q_INDEX, ignore=404)
+	es.indices.create(
+		index=Q_INDEX,
+		body={
+			'mappings':{},
+			'settings':{},
+		})
+
+	helpers.bulk(es, questions_to_index(all_questions()))
+
+def questions_to_index(questions):
+	# Generator function that yields data objects
+	for q in questions:
+		yield {
+			'_op_type': 'create',
+			'_index': Q_INDEX,
+			'_type': Q_DOC_T,
+			'_id': q.questionId,
+			'_source': {
+				'date': q.date,
+				'user': q.userId,
+				'category': q.categoryId,
+				'question': q.question,
+				'description': q.description
+			}
+		}
+
+if __name__ == '__main__':
+	main()
