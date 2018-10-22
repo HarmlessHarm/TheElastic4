1d05abbcb3b7a5ba290665201495edde67e25da5
Harm Manders <harmmanders@gmail.com>
2018-10-22 23:06:48 +0200

    Basic search results are now live!!

--- elasticapp/app/app.py
+++ elasticapp/app/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 
+from elasticapp.app.search import search
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	msg = "HOOI DIT IS DATAA"
-	return render_template('index.html', data=msg)
+	search_terms = [
+		'konijn',
+		'nederlandse',
+		'nederndse',
+	]
+
+	num_results = 9
+	searched_questions = [(t, search(t, num_results)) for t in search_terms]
+	return render_template('index.html', searched_questions=searched_questions)
---
+++ elasticapp/app/search.py
@@ -0,0 +1,44 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+from typing import List
+
+from elasticapp.constants import *
+
+HEADERS = {'content-type': 'application/json'}
+
+class SearchResult(object):
+	"""docstring for SearchResult"""
+	def __init__(self, qid, question, description):
+		super(SearchResult, self).__init__()
+		self.id = qid
+		self.question = question
+		self.description = description
+
+	def from_doc(doc) -> 'SearchResult':
+		return SearchResult(
+				qid = doc.meta.id,
+				question = doc.question,
+				description = doc.description,
+			)
+		pass
+
+
+def search(term:str, count:int) -> List[SearchResult]:
+	client = Elasticsearch()
+
+	client.transport.connection_pool.connection.headers.update(HEADERS)
+
+	s = Search(using=client, index=Q_INDEX, doc_type=Q_DOC_T)
+	q_query = {
+		'match': {
+			'question': {
+				'query': term,
+				'operator': 'and',
+				'fuzziness': 'AUTO',
+			}
+		}
+	}
+
+	docs = s.query(q_query)[:count].execute()
+
+	return [SearchResult.from_doc(d) for d in docs]
