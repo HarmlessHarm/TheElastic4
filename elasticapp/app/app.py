12eb867f4d1d3f8410272a119ad1fe50943d8fc5
Sander Hansen <mail@sanderhansen.nl>
2018-10-25 08:51:00 +0200

    Started on timeline

--- elasticapp/app/app.py
+++ elasticapp/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from pprint import pprint
 from elasticapp.app.search import search
 
 app = Flask(__name__)
@@ -25,3 +25,13 @@ def search_question():
 	num_results = 50
 	results = [(query, search(query, num_results))]
 	return render_template('index.html', results=results,search_term=query)
+
+
+@app.route('/timeline', methods=['GET','POST'])
+def get_timeline():
+
+	query = request.args.get('timeline')
+	num_results = 50
+	results = [(query, search(query, num_results))]
+	pprint(vars(search(query, 1)[0]))
+	return render_template('timeline.html', results=results,search_term=query)
