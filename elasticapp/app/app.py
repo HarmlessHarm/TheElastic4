202c2783df1061a6d9ff85e34f904a50827614c2
Harm Manders <harmmanders@gmail.com>
2018-10-28 20:25:11 +0100

    merge fix again

--- elasticapp/app/app.py
+++ elasticapp/app/app.py
@@ -29,7 +29,7 @@ def search_question():
 		'timeline': timeline,
 		'wordcloud': wordcloud,
 		'count': count,
-		'range': '{} - {}'.format(str(page * 25 + 1), str(page * 25 + 25))
+		'range': '{} - {}'.format(str(page * 10 + 1), str(page * 10 + 10))
 	}
 	return render_template('index.html', data=data, search_term=query)
 
